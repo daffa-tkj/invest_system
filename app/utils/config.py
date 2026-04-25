fb7b6a0eaeb84d784473debe779d893325ed2da8
daffa-tkj <109427521+daffa-tkj@users.noreply.github.com>
2026-04-25 09:25:08 +0700

    Update config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -7,57 +7,161 @@ load_dotenv()
 # ========== DATABASE ==========
 DB_PATH = os.getenv("DB_PATH", "data/invest.db")
 
-# ========== SAHAM ==========
-STOCKS = [
-    # BANK
-    'BBCA.JK', 'BBRI.JK', 'BMRI.JK', 'BBNI.JK', 'BRIS.JK',
-    'BNGA.JK', 'BNLI.JK', 'AGRO.JK', 'ARTO.JK', 'BBTN.JK',
-    'BDMN.JK', 'BJBR.JK', 'BJTM.JK', 'BMAS.JK', 'BTPN.JK',
-    'MAYA.JK', 'MEGA.JK', 'NISP.JK', 'NOBU.JK', 'PNBN.JK',
-    
-    # TELEKOMUNIKASI
-    'TLKM.JK', 'ISAT.JK', 'EXCL.JK', 'FREN.JK', 'TOWR.JK', 'TBIG.JK', 'MTEL.JK',
-    
-    # ENERGI
-    'ADRO.JK', 'ITMG.JK', 'PTBA.JK', 'MEDC.JK', 'PGAS.JK', 'BUMI.JK', 'BYAN.JK',
-    'DOID.JK', 'INDY.JK', 'HRUM.JK', 'ADMR.JK', 'ENRG.JK', 'AKRA.JK', 'ESSA.JK',
-    
-    # CONSUMER GOODS
-    'UNVR.JK', 'ICBP.JK', 'INDF.JK', 'MYOR.JK', 'GGRM.JK', 'HMSP.JK', 'WIIM.JK',
-    'DLTA.JK', 'MLBI.JK', 'ULTJ.JK', 'ROTI.JK', 'KEJU.JK', 'CAMP.JK', 'SKBM.JK', 'ADES.JK',
-    
-    # PROPERTY
-    'BSDE.JK', 'CTRA.JK', 'PWON.JK', 'SMRA.JK', 'LPKR.JK', 'DMAS.JK', 'DILD.JK',
-    'MDLN.JK', 'KIJA.JK', 'RISE.JK', 'TARA.JK',
-    
-    # INFRASTRUKTUR
-    'JSMR.JK', 'WIKA.JK', 'PTPP.JK', 'ADHI.JK', 'WSKT.JK', 'INTP.JK', 'SMCB.JK', 'SMGR.JK',
-    
-    # RETAIL
-    'AMRT.JK', 'ACES.JK', 'RALS.JK', 'MAPI.JK', 'ERAA.JK', 'LPPF.JK', 'JPFA.JK', 'CPIN.JK', 'MAIN.JK',
-    
-    # OTOMOTIF
-    'ASII.JK', 'AUTO.JK', 'GDYR.JK', 'GJTL.JK', 'LPIN.JK', 'SMSM.JK',
-    
-    # FARMASI
-    'KAEF.JK', 'SIDO.JK', 'KLBF.JK', 'MERK.JK', 'PYFA.JK', 'TSPC.JK', 'HEAL.JK', 'SILO.JK', 'MIKA.JK',
-    
-    # TAMBANG
-    'ANTM.JK', 'INCO.JK', 'TINS.JK', 'MDKA.JK', 'BRMS.JK', 'CITA.JK', 'NCKL.JK', 'PBID.JK',
-    
-    # TEKNOLOGI
-    'DIVA.JK', 'BELI.JK', 'BUKA.JK', 'GOTO.JK',
-    'BRPT.JK', 'BREN.JK', 'TPIA.JK', 'CUAN.JK', 'PTRO.JK', 'CDIA.JK',
+# ========== SAHAM BERDASARKAN KAPITALISASI ==========
+
+# === LARGE CAP (Market Cap > Rp50 Triliun) ===
+LARGE_CAP_STOCKS = [
+    'BBCA.JK', 'BBRI.JK', 'BMRI.JK', 'BBNI.JK', 'TLKM.JK', 'ASII.JK', 
+    'ADRO.JK', 'UNVR.JK', 'ICBP.JK', 'INDF.JK', 'CPIN.JK', 'JPFA.JK',
+    'PGAS.JK', 'SMGR.JK', 'JSMR.JK', 'KLBF.JK', 'MYOR.JK', 'GGRM.JK',
+    'HMSP.JK', 'TOWR.JK', 'BRPT.JK', 'TPIA.JK', 'BREN.JK', 'MDKA.JK',
+    'AMMN.JK', 'ARTO.JK', 'AGRO.JK', 'BUKA.JK', 'GOTO.JK'
 ]
 
-# ========== PREDIKSI ==========
+# === MID CAP (Rp10 Triliun - Rp50 Triliun) ===
+MID_CAP_STOCKS = [
+    'BRIS.JK', 'BNGA.JK', 'BTPS.JK', 'MAPI.JK', 'ERAA.JK', 'ACES.JK',
+    'AMRT.JK', 'LSIP.JK', 'AALI.JK', 'SIMP.JK', 'PTBA.JK', 'ITMG.JK',
+    'BYAN.JK', 'MEDC.JK', 'ENRG.JK', 'ESSA.JK', 'AKRA.JK', 'CUAN.JK',
+    'PTRO.JK', 'CDIA.JK', 'TBIG.JK', 'MTEL.JK', 'EXCL.JK', 'ISAT.JK',
+    'FREN.JK', 'BSDE.JK', 'CTRA.JK', 'PWON.JK', 'LPKR.JK', 'DMAS.JK',
+    'DILD.JK', 'KIJA.JK', 'SILO.JK', 'MIKA.JK', 'HEAL.JK', 'SIDO.JK',
+    'KAEF.JK', 'MERK.JK', 'PYFA.JK', 'TSPC.JK', 'ASSA.JK', 'UNTR.JK',
+    'AUTO.JK', 'GDYR.JK', 'SMSM.JK', 'ANTM.JK', 'INCO.JK', 'TINS.JK',
+    'BRMS.JK', 'NCKL.JK', 'PBID.JK', 'RALS.JK', 'LPPF.JK'
+]
+
+# === SMALL CAP (Rp1 Triliun - Rp10 Triliun) ===
+SMALL_CAP_STOCKS = [
+    'BNLI.JK', 'MAYA.JK', 'MEGA.JK', 'NISP.JK', 'NOBU.JK', 'PNBN.JK',
+    'BJBR.JK', 'BJTM.JK', 'BMAS.JK', 'BTPN.JK', 'BBTN.JK', 'AGRO.JK',
+    'BNLI.JK', 'INPC.JK', 'ERAL.JK', 'WIIM.JK', 'DLTA.JK', 'MLBI.JK',
+    'ULTJ.JK', 'ROTI.JK', 'KEJU.JK', 'CAMP.JK', 'SKBM.JK', 'ADES.JK',
+    'DOID.JK', 'INDY.JK', 'HRUM.JK', 'ADMR.JK', 'BUMI.JK', 'DEWA.JK',
+    'BNBR.JK', 'VKTR.JK', 'ALII.JK', 'SMRA.JK', 'TARA.JK', 'MDLN.JK',
+    'RISE.JK', 'WIKA.JK', 'PTPP.JK', 'ADHI.JK', 'WSKT.JK', 'INTP.JK',
+    'SMCB.JK', 'LPIN.JK', 'MAIN.JK', 'DIVA.JK', 'BELI.JK', 'SSIA.JK',
+    'SMMA.JK', 'BSIM.JK', 'DSNG.JK', 'INKP.JK', 'TKIM.JK', 'SMAR.JK',
+    'DSSA.JK', 'GEMS.JK', 'MORA.JK', 'ASDM.JK', 'TECH.JK'
+]
+
+# === MICRO CAP (Market Cap < Rp1 Triliun) ===
+MICRO_CAP_STOCKS = [
+    'FAPA.JK', 'PANI.JK', 'DNET.JK', 'BTPN.JK', 'MORA.JK', 'ASDM.JK',
+    'INPC.JK', 'ERAL.JK', 'LPIN.JK', 'MAIN.JK', 'BELI.JK', 'DIVA.JK',
+    'SSIA.JK', 'SMMA.JK', 'BSIM.JK', 'DSNG.JK', 'INKP.JK', 'TKIM.JK',
+    'SMAR.JK', 'DSSA.JK', 'GEMS.JK', 'TECH.JK', 'VKTR.JK', 'ALII.JK',
+    'BNBR.JK', 'DEWA.JK', 'BUMI.JK', 'ADMR.JK', 'HRUM.JK', 'INDY.JK',
+    'DOID.JK', 'CAMP.JK', 'KEJU.JK', 'ROTI.JK', 'ULTJ.JK', 'DLTA.JK',
+    'WIIM.JK', 'PNBN.JK', 'NOBU.JK', 'MEGA.JK', 'MAYA.JK', 'BNLI.JK'
+]
+
+# === GABUNGAN SEMUA SAHAM ===
+ALL_STOCKS = LARGE_CAP_STOCKS + MID_CAP_STOCKS + SMALL_CAP_STOCKS + MICRO_CAP_STOCKS
+
+# ========== SAHAM BERDASARKAN SEKTOR ==========
+
+# Perbankan
+BANK_STOCKS = [
+    'BBCA.JK', 'BBRI.JK', 'BMRI.JK', 'BBNI.JK', 'BRIS.JK', 'BNGA.JK',
+    'BTPS.JK', 'BNLI.JK', 'MAYA.JK', 'MEGA.JK', 'NISP.JK', 'NOBU.JK',
+    'PNBN.JK', 'BJBR.JK', 'BJTM.JK', 'BMAS.JK', 'BTPN.JK', 'BBTN.JK',
+    'AGRO.JK', 'ARTO.JK', 'INPC.JK'
+]
+
+# Telekomunikasi & Media
+TELCO_STOCKS = [
+    'TLKM.JK', 'ISAT.JK', 'EXCL.JK', 'FREN.JK', 'TOWR.JK', 'TBIG.JK',
+    'MTEL.JK', 'MORA.JK', 'EMTK.JK', 'SCMA.JK'
+]
+
+# Energi & Tambang
+ENERGY_STOCKS = [
+    'ADRO.JK', 'PTBA.JK', 'ITMG.JK', 'BYAN.JK', 'DOID.JK', 'INDY.JK',
+    'HRUM.JK', 'ADMR.JK', 'BUMI.JK', 'DEWA.JK', 'ENRG.JK', 'ESSA.JK',
+    'AKRA.JK', 'MEDC.JK', 'PGAS.JK', 'MDKA.JK', 'ANTM.JK', 'INCO.JK',
+    'TINS.JK', 'BRMS.JK', 'NCKL.JK', 'PBID.JK', 'CITA.JK', 'PTRO.JK'
+]
+
+# Consumer Goods
+CONSUMER_STOCKS = [
+    'UNVR.JK', 'ICBP.JK', 'INDF.JK', 'MYOR.JK', 'GGRM.JK', 'HMSP.JK',
+    'WIIM.JK', 'DLTA.JK', 'MLBI.JK', 'ULTJ.JK', 'ROTI.JK', 'KEJU.JK',
+    'CAMP.JK', 'SKBM.JK', 'ADES.JK', 'CPIN.JK', 'JPFA.JK', 'MAIN.JK',
+    'SIDO.JK', 'KAEF.JK', 'KLBF.JK', 'MERK.JK', 'PYFA.JK', 'TSPC.JK'
+]
+
+# Property & Real Estate
+PROPERTY_STOCKS = [
+    'BSDE.JK', 'CTRA.JK', 'PWON.JK', 'SMRA.JK', 'LPKR.JK', 'DMAS.JK',
+    'DILD.JK', 'MDLN.JK', 'KIJA.JK', 'RISE.JK', 'TARA.JK', 'SMMA.JK',
+    'BSIM.JK', 'DSSA.JK', 'GEMS.JK'
+]
+
+# Infrastruktur & Konstruksi
+INFRA_STOCKS = [
+    'JSMR.JK', 'WIKA.JK', 'PTPP.JK', 'ADHI.JK', 'WSKT.JK', 'INTP.JK',
+    'SMCB.JK', 'SMGR.JK', 'SSIA.JK', 'TPIA.JK', 'BREN.JK', 'CUAN.JK'
+]
+
+# Retail & Distribusi
+RETAIL_STOCKS = [
+    'AMRT.JK', 'ACES.JK', 'RALS.JK', 'MAPI.JK', 'ERAA.JK', 'LPPF.JK',
+    'LPIN.JK', 'MAIN.JK', 'ASSA.JK', 'FAPA.JK'
+]
+
+# Otomotif
+AUTOMOTIVE_STOCKS = [
+    'ASII.JK', 'AUTO.JK', 'GDYR.JK', 'LPIN.JK', 'SMSM.JK', 'GJTL.JK'
+]
+
+# Healthcare
+HEALTHCARE_STOCKS = [
+    'SILO.JK', 'MIKA.JK', 'HEAL.JK', 'KAEF.JK', 'KLBF.JK', 'MERK.JK',
+    'PYFA.JK', 'TSPC.JK', 'SIDO.JK'
+]
+
+# Teknologi & Digital
+TECH_STOCKS = [
+    'DIVA.JK', 'BELI.JK', 'BUKA.JK', 'GOTO.JK', 'DNET.JK', 'PANI.JK'
+]
+
+# ========== SAHAM KONGLOMERAT ==========
+
+DJARUM_GROUP = {
+    'name': 'Grup Djarum (Robert Budi Hartono)',
+    'stocks': ['BBCA.JK', 'TOWR.JK', 'MTEL.JK', 'SSIA.JK', 'HEAL.JK'],
+    'desc': 'Konglomerat rokok terbesar, pemilik BCA, tower telekomunikasi, dan properti'
+}
+
+SALIM_GROUP = {
+    'name': 'Grup Salim (Anthony Salim)',
+    'stocks': ['INDF.JK', 'ICBP.JK', 'AGRO.JK', 'SIMP.JK', 'LSIP.JK', 'FAPA.JK'],
+    'desc': 'Konglomerat makanan terbesar (Indomie), perkebunan'
+}
+
+SINARMAS_GROUP = {
+    'name': 'Grup Sinar Mas (Keluarga Widjaja)',
+    'stocks': ['SMMA.JK', 'BSIM.JK', 'BSDE.JK', 'DMAS.JK', 'DSSA.JK', 'GEMS.JK', 'INKP.JK', 'TKIM.JK', 'SMAR.JK', 'DSNG.JK'],
+    'desc': 'Konglomerat properti, energi, pulp & kertas, dan perkebunan'
+}
+
+ASTRA_GROUP = {
+    'name': 'Grup Astra (Keluarga Soeryadjaya)',
+    'stocks': ['ASII.JK', 'UNTR.JK', 'AALI.JK', 'AUTO.JK', 'ASSA.JK'],
+    'desc': 'Konglomerat otomotif & alat berat terbesar di Indonesia'
+}
+
+# ========== SAHAM PREDIKSI ==========
 FORECAST_STOCKS = ['BBCA.JK', 'BBRI.JK', 'TLKM.JK', 'ASII.JK', 'ADRO.JK']
-FORECAST_PERIOD = 30
-TRAIN_DAYS = 730
 
 # ========== TELEGRAM ==========
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+
+# ========== PREDIKSI ==========
+FORECAST_PERIOD = 30
+TRAIN_DAYS = 730
 
 # ========== KATEGORI BANDARMOLOGY ==========
 FOREIGN_FLOW_UPTREN = ['BBCA.JK', 'BMRI.JK', 'TLKM.JK', 'ASII.JK', 'ADRO.JK', 'MDKA.JK', 'ISAT.JK', 'TOWR.JK']
@@ -73,47 +177,30 @@ DIVIDEN_STOCKS = [
     'ASII.JK', 'INDF.JK', 'ICBP.JK', 'UNVR.JK', 'MYOR.JK', 'ACES.JK', 'AMRT.JK',
     'JSMR.JK', 'PGAS.JK', 'SMGR.JK', 'KLBF.JK', 'SIDO.JK'
 ]
+
 CHEAP_BIG_CAPS_DIVIDEN = ['TLKM.JK', 'BBRI.JK', 'BMRI.JK', 'ASII.JK', 'ADRO.JK', 'PTBA.JK', 'PGAS.JK', 'JSMR.JK']
 CHEAP_MID_CAPS_DIVIDEN = ['ACES.JK', 'AMRT.JK', 'BRIS.JK', 'INDF.JK', 'ICBP.JK', 'MYOR.JK', 'TOWR.JK', 'TBIG.JK']
 CHEAP_SMALL_CAPS_DIVIDEN = ['DMAS.JK', 'SUNI.JK', 'CARS.JK', 'ESSA.JK', 'ADMR.JK', 'WIKA.JK', 'PTPP.JK', 'ADHI.JK', 'WSKT.JK']
 TOP_DIVIDEN = ['ADRO.JK', 'PTBA.JK', 'ITMG.JK', 'BYAN.JK', 'DOID.JK', 'HRUM.JK', 'INDY.JK']
-BANDARMOLOGY_DIVIDEN = ['ADRO.JK', 'PTBA.JK', 'TLKM.JK', 'BRIS.JK', 'ACES.JK', 'DMAS.JK', 'CARS.JK', 'SUNI.JK', 'ESSA.JK', 'ADMR.JK']
+BANDARMOLOGY_DIVIDEN = [
+    'ADRO.JK', 'PTBA.JK', 'TLKM.JK', 'BRIS.JK', 'ACES.JK', 'DMAS.JK', 'CARS.JK', 'SUNI.JK', 'ESSA.JK', 'ADMR.JK'
+]
 
-# ========== GRUP KONGLOMERAT ==========
-DJARUM_GROUP = {
-    'name': 'Grup Djarum (Robert Budi Hartono)',
-    'stocks': ['BBCA.JK', 'TOWR.JK', 'MTEL.JK', 'SSIA.JK', 'HEAL.JK'],
-    'desc': 'Konglomerat rokok terbesar, pemilik BCA'
-}
-SALIM_GROUP = {
-    'name': 'Grup Salim (Anthony Salim)',
-    'stocks': ['INDF.JK', 'ICBP.JK', 'AMMN.JK', 'PANI.JK', 'DNET.JK', 'FAPA.JK', 'SIMP.JK', 'LSIP.JK', 'AGRO.JK'],
-    'desc': 'Konglomerat makanan terbesar (Indomie)'
-}
-SINARMAS_GROUP = {
-    'name': 'Grup Sinar Mas (Keluarga Widjaja)',
-    'stocks': ['SMMA.JK', 'BSIM.JK', 'BSDE.JK', 'DMAS.JK', 'DSSA.JK', 'GEMS.JK', 'INKP.JK', 'TKIM.JK', 'SMAR.JK', 'DSNG.JK'],
-    'desc': 'Konglomerat properti, energi, pulp & kertas'
-}
-PRAJOGO_GROUP = {
-    'name': 'Grup Barito (Prajogo Pangestu)',
-    'stocks': ['BRPT.JK', 'BREN.JK', 'TPIA.JK', 'CUAN.JK', 'PTRO.JK', 'CDIA.JK'],
-    'desc': 'Konglomerat energi & petrokimia'
-}
-ASTRA_GROUP = {
-    'name': 'Grup Astra (Keluarga Soeryadjaya)',
-    'stocks': ['ASII.JK', 'UNTR.JK', 'AALI.JK', 'AUTO.JK', 'ASSA.JK'],
-    'desc': 'Konglomerat otomotif & alat berat'
-}
-BAKRIE_GROUP = {
-    'name': 'Grup Bakrie (Keluarga Bakrie)',
-    'stocks': ['BUMI.JK', 'BRMS.JK', 'DEWA.JK', 'ENRG.JK', 'BNBR.JK', 'VKTR.JK', 'ALII.JK'],
-    'desc': 'Konglomerat energi & tambang'
-}
-LIPPO_GROUP = {
-    'name': 'Grup Lippo (James Riady)',
-    'stocks': ['LPKR.JK', 'LPCK.JK', 'SILO.JK', 'MPPA.JK', 'LPPF.JK', 'MLPT.JK', 'BUKA.JK'],
-    'desc': 'Konglomerat properti, kesehatan, ritel'
+# ========== SEMUA GRUP KONGLOMERAT ==========
+ALL_KONGLOMERAT_GROUPS = [
+    DJARUM_GROUP, SALIM_GROUP, SINARMAS_GROUP, ASTRA_GROUP
+]
+
+# ========== TIMEFRAME SCALPING ==========
+TIMEFRAMES = {
+    "1 Jam": "60m",
+    "4 Jam": "240m",
+    "Daily": "1d"
 }
 
-ALL_KONGLOMERAT_GROUPS = [DJARUM_GROUP, SALIM_GROUP, SINARMAS_GROUP, PRAJOGO_GROUP, ASTRA_GROUP, BAKRIE_GROUP, LIPPO_GROUP]
+# ========== PARAMETER SCALPING ==========
+SCALPING_ATR_PERIOD = 14
+SCALPING_STOCH_RSI_PERIOD = 14
+SCALPING_EMA_SHORT = 5
+SCALPING_EMA_MEDIUM = 10
+SCALPING_EMA_LONG = 20
